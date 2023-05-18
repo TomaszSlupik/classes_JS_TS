f10ce5c391bbc09f4188876c87d6757d6cae18d5
TomaszSlupik <slupiktomasz@gmail.com>
2023-05-18 07:44:29 +0200

    change

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,22 @@ class SwimmingPool {
 }
 const pool = new SwimmingPool('Basen WUM');
 console.log(`Dzisiaj pływałem na: ${pool.namePool}`);
+// Zmiana wartości 
+class NumberWatch {
+    constructor(number) {
+        this.number = number;
+    }
+}
+const watch = new NumberWatch(3);
+watch.number = 10;
+console.log(watch.number);
+// Readonly
+class WalkTheDog {
+    constructor(dog, kilometers) {
+        this.dog = dog;
+        this.kilometers = kilometers;
+    }
+}
+const walk = new WalkTheDog('Husky', 10);
+walk.kilometers = 15;
+console.log(`Wczoraj razem z ${walk.dog} zrobiliśmy ${walk.kilometers} km`);
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,38 @@ class SwimmingPool {
 const pool =  new SwimmingPool ('Basen WUM')
 
 console.log(`Dzisiaj pływałem na: ${pool.namePool}`)
+
+
+// Zmiana wartości 
+class NumberWatch {
+    number: number
+
+    constructor(number: number) {
+        this.number = number
+    }
+}
+
+const watch = new NumberWatch(3)
+
+watch.number = 10
+
+console.log(watch.number)
+
+
+// Readonly
+class WalkTheDog {
+    readonly dog: string
+    kilometers: number
+
+    constructor(dog:string, kilometers: number) {
+        this.dog = dog
+        this.kilometers = kilometers
+    }
+}
+
+const walk = new WalkTheDog ('Husky', 10)
+walk.kilometers = 15
+
+console.log(`Wczoraj razem z ${walk.dog} zrobiliśmy ${walk.kilometers} km`)
+
+
