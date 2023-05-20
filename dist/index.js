7b04fea249c5132b874455848baea1f3ccdbfabb
TomaszSlupik <slupiktomasz@gmail.com>
2023-05-20 10:42:42 +0200

    change

--- dist/index.js
+++ dist/index.js
@@ -37,3 +37,52 @@ class Clerk {
 const clerk = new Clerk('Tom');
 console.log(clerk.nameClerk);
 console.log(clerk.workClerk);
+// Private
+class Phone {
+    constructor(production) {
+        this.namePhone = 'Iphone';
+        this.production = production;
+    }
+    infoUser() {
+        console.log(`${this.namePhone} ma ${this.production} lat`);
+    }
+}
+const iphone = new Phone(5);
+console.log(iphone.infoUser());
+// ZAPIS SKRÓCONY W KONSTRUKTORZE 
+class Glass {
+    constructor(nameGlass, year) {
+        this.nameGlass = nameGlass;
+        this.year = year;
+    }
+}
+const glass = new Glass('Ray-ban', 3);
+console.log(glass);
+// Gettery 
+class Weather {
+    constructor(kind) {
+        this.kind = kind;
+    }
+    get log() {
+        return `Dzisiaj jest ${this.kind}`;
+    }
+}
+const weather = new Weather('słonecznie');
+console.log(weather.log);
+class NumberCheck {
+    constructor(checkName, _score) {
+        this.checkName = checkName;
+        this._score = _score;
+    }
+    set scoreNumber(newScore) {
+        if (newScore > 0) {
+            this._score = newScore;
+        }
+        else {
+            throw new Error('Błąd');
+        }
+    }
+}
+const numberCheck = new NumberCheck('Testowanie liczb', 20);
+numberCheck.scoreNumber = 1;
+console.log(numberCheck);
--- src/index.ts
+++ src/index.ts
@@ -58,3 +58,67 @@ const clerk = new Clerk ('Tom')
 console.log(clerk.nameClerk)
 console.log(clerk.workClerk)
 
+// Private
+class Phone {
+
+    production: number
+    private namePhone: string = 'Iphone'
+
+    constructor (production: number) {
+        this.production = production
+    }
+
+    infoUser () {
+        console.log(`${this.namePhone} ma ${this.production} lat`)
+    }
+}
+
+const iphone = new Phone(5)
+
+console.log(iphone.infoUser())
+
+// ZAPIS SKRÓCONY W KONSTRUKTORZE 
+class Glass {
+    constructor(public nameGlass: string, public year: number) {}
+}
+const glass = new Glass('Ray-ban', 3)
+console.log(glass)
+
+// Gettery 
+class Weather {
+    public kind: string
+
+    constructor (kind:string) {
+        this.kind = kind
+    }
+
+    get log () {
+        return `Dzisiaj jest ${this.kind}`
+    }
+}
+
+const weather =  new Weather ('słonecznie')
+
+console.log(weather.log)
+
+class NumberCheck {
+    constructor(public checkName: string, private _score: number) {}
+
+    set scoreNumber (newScore: number) {
+        if (newScore > 0) {
+            this._score =  newScore
+        }
+        else {
+            throw new Error ('Błąd')
+        }
+    }
+}
+
+const numberCheck = new NumberCheck('Testowanie liczb', 20)
+
+numberCheck.scoreNumber = 1
+console.log(numberCheck)
+
+
+
+
